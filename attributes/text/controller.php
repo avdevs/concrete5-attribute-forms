d94a6a80ddb8de4d90e42861bd7ede39b58cfbce
bhavin_avdevs <bhavin@avdevs.com>
2016-04-22 17:15:43 +0530

    Text attribute override for place holder

---
+++ attributes/text/controller.php
@@ -0,0 +1,122 @@
+<?php
+namespace Concrete\Package\AttributeForms\Attribute\Text;
+
+use Core;
+use Database;
+use \Concrete\Core\Foundation\Object;
+use \Concrete\Core\Attribute\Controller as AttributeTypeController;
+use \Concrete\Core\Attribute\DefaultController as DefaultAttributeTypeController;
+
+class Controller extends AttributeTypeController  {
+
+    protected $searchIndexFieldDefinition = array('type' => 'text', 'options' => array('default' => null, 'notnull' => false));
+
+    public $helpers = array('form');
+
+    public function saveKey($data)
+    {
+        $data += array(
+            'akTextPlaceholder' => null,
+        );
+        $akTextPlaceholder = $data['akTextPlaceholder'];
+
+        $this->setDisplayMode($akTextPlaceholder);
+    }
+
+    public function form() {
+        $this->load();
+        if (is_object($this->attributeValue)) {
+            $value = Core::make('helper/text')->entities($this->getAttributeValue()->getValue());
+        }
+        print Core::make('helper/form')->text($this->field('value'), $value, array( 'placeholder' => $this->akTextPlaceholder));
+    }
+
+    public function composer() {
+
+        if (is_object($this->attributeValue)) {
+            $value = Core::make('helper/text')->entities($this->getAttributeValue()->getValue());
+        }
+        print Core::make('helper/form')->text($this->field('value'), $value, array('class' => 'span5'));
+    }
+
+    public function searchForm($list)
+    {
+        $list->filterByAttribute($this->attributeKey->getAttributeKeyHandle(), '%' . $this->request('value') . '%', 'like');
+
+        return $list;
+    }
+
+    public function search()
+    {
+        $f = Core::make('helper/form');
+        print $f->text($this->field('value'), $this->request('value'));
+    }
+
+    public function setDisplayMode($akTextPlaceholder)
+    {
+        $db = Database::connection();
+        $ak = $this->getAttributeKey();
+
+        $db->Replace('atTextSettings', array(
+            'akID' => $ak->getAttributeKeyID(),
+            'akTextPlaceholder' => $akTextPlaceholder
+        ), array('akID'), true);
+    }
+
+    // should have to delete the at thing
+    public function deleteKey()
+    {
+        $db = Database::connection();
+        $arr = $this->attributeKey->getAttributeValueIDList();
+        foreach ($arr as $id) {
+            $db->Execute('delete from atDefault where avID = ?', array($id));
+        }
+
+        $db->Execute('delete from atTextSettings where akID = ?', array($this->attributeKey->getAttributeKeyID()));
+    }
+
+    public function type_form()
+    {
+        $this->load();
+    }
+
+    protected function load()
+    {
+        $ak = $this->getAttributeKey();
+        if (!is_object($ak)) {
+            return false;
+        }
+
+        $db = Database::connection();
+        $row = $db->GetRow('select akTextPlaceholder from atTextSettings where akID = ?', array($ak->getAttributeKeyID()));
+        $this->akTextPlaceholder = $row['akTextPlaceholder'];
+
+        $this->set('akTextPlaceholder', $this->akTextPlaceholder);
+    }
+
+    public function exportKey($akey)
+    {
+        $this->load();
+        $akey->addChild('type')->addAttribute('mode', $this->akTextPlaceholder);
+
+        return $akey;
+    }
+
+    public function importKey($akey)
+    {
+        if (isset($akey->type)) {
+            $data['akTextPlaceholder'] = $akey->type['mode'];
+            $this->saveKey($data);
+        }
+    }
+
+    public function duplicateKey($newAK)
+    {
+        $this->load();
+        $db = Database::connection();
+        $db->Replace('atTextSettings', array(
+            'akID' => $newAK->getAttributeKeyID(),
+            'akTextPlaceholder' => $this->akTextPlaceholder,
+        ), array('akID'), true);
+    }
+}
